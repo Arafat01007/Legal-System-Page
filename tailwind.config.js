78c65f195750e9b8fedb0823a82e9f8fb8c070bb
Arafat01007 <arafatfahadbro@gmail.com>
2024-11-02 16:00:46 +0600

    added banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'banner': "url('')",
+      }
+    },
   },
   plugins: [
     require('daisyui'),
